c804619f28077f61a25f2c48bbd769496cb7a1c2
AwadhootK <awadhootk6@gmail.com>
2024-09-07 23:22:02 +0530

    changes

--- utils/redis_service.py
+++ utils/redis_service.py
@@ -3,8 +3,13 @@ import os
 from redis import Redis
 
 
-
 def get_redis_connection():
+    print("REDIS HOST = " + os.getenv("REDIS_HOST"))
+    print("REDIS PORT = " + os.getenv("REDIS_PORT"))
+
+    print("CHROMA HOST = " + os.getenv("CHROMA_HOST"))
+    print("CHROMA PORT = " + os.getenv("CHROMA_PORT"))
+
     return Redis(
         host=os.getenv("REDIS_HOST"),
         port=os.getenv("REDIS_PORT"),

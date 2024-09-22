39502f3d2040515ef30dcf3e4fdbea71a78733b7
AwadhootK <awadhootk6@gmail.com>
2024-09-22 16:20:11 +0530

    k8s

--- src/main/java/com/example/springapigateway/GatewayConfig.java
+++ src/main/java/com/example/springapigateway/GatewayConfig.java
@@ -1,37 +1,35 @@
 package com.example.springapigateway;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class GatewayConfig {
 
-    private final AuthenticationFilter filter;
+        private final AuthenticationFilter filter;
 
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        final String RAG_URI = System.getenv("RAG_HOST")+":"+System.getenv("RAG_PORT")+"/";
-        final String AUTH_URI = System.getenv("AUTH_HOST") + ":" + System.getenv("AUTH_PORT") + "/";
-        
-        log.info(AUTH_URI);
-        log.info(RAG_URI);
-        
-        return builder.routes()
-                .route("auth-service", r -> r.path("/auth/**")
-                        .filters(f -> f.filter(filter)
-                                .stripPrefix(1)) // Strip the first path segment (/auth)
-                        .uri(AUTH_URI))
-                .route("rag-service", r -> r.path("/rag/**")
-                        .filters(f -> f.filter(filter).stripPrefix(1))
-                        .uri(RAG_URI)
-                )
-                .build();
-    }
+        @Bean
+        public RouteLocator routes(RouteLocatorBuilder builder) {
+                final String RAG_URI = "http://rag-service:8000/";
+                // System.getenv("RAG_HOST")+":"+System.getenv("RAG_PORT")+"/";
+                final String AUTH_URI = "http://auth-service:8500/";
+                // System.getenv("AUTH_HOST") + ":" + System.getenv("AUTH_PORT") + "/";
+
+                return builder.routes()
+                                .route("auth-service", r -> r.path("/auth/**")
+                                                .filters(f -> f.filter(filter)
+                                                                .stripPrefix(1)) // Strip the first path segment (/auth)
+                                                .uri(AUTH_URI))
+                                .route("rag-service", r -> r.path("/rag/**")
+                                                .filters(f -> f.filter(filter).stripPrefix(1))
+                                                .uri(RAG_URI))
+                                .build();
+        }
 }

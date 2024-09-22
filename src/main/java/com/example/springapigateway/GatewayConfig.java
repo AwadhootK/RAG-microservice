896fa3478c627b905ba4e8e3b63293a697679882
AwadhootK <awadhootk6@gmail.com>
2024-09-22 15:53:24 +0530

    k8s

--- src/main/java/com/example/springapigateway/GatewayConfig.java
+++ src/main/java/com/example/springapigateway/GatewayConfig.java
@@ -14,14 +14,17 @@ public class GatewayConfig {
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
+        final String RAG_URI = System.getenv("RAG_HOST")+":"+System.getenv("RAG_PORT")+"/";
+        final String AUTH_URI = System.getenv("AUTH_HOST")+":"+System.getenv("AUTH_PORT")+"/";
+
         return builder.routes()
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(filter)
                                 .stripPrefix(1)) // Strip the first path segment (/auth)
-                        .uri("http://127.0.0.1:8500/"))
+                        .uri(AUTH_URI))
                 .route("rag-service", r -> r.path("/rag/**")
                         .filters(f -> f.filter(filter).stripPrefix(1))
-                        .uri("http://127.0.0.1:8500/")
+                        .uri(RAG_URI)
                 )
                 .build();
     }
